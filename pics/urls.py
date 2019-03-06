d4fa7c12742bd975606a061c7a9d21fea9c70ee0
Mugo-Ndungu <twinnymugo@gmail.com>
2019-03-06 15:52:47 +0300

    lol

--- pics/urls.py
+++ pics/urls.py
@@ -4,12 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$',views.pics_of_day,name='picsToday'),
-    url('^archives/(\d{4}-\d{2}-\d{2})/$',
+    url(r'^/', views.pics_of_day, name='picsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',
         views.past_days_pics, name='pastPics'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r"^main/", views.page, name="location"),
-    url(r"^location/(\w+)", views.location, name="locations")
+    url(r'^main/', views.page, name="location"),
+    url(r'^location/', views.location, name="locations")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
